a6ca2342b9f894e023f2bb8ed97f8f68ced80909
Lefti90 <ilkka.saekkinen@gmail.com>
2023-05-23 23:14:45 +0300

    1_7 done

--- osa1/unicafe/1_6_unicafe/src/App.js
+++ osa1/unicafe/1_6_unicafe/src/App.js
@@ -22,14 +22,21 @@ const App = () => {
     setBad(bad+1)
   }
 
+  const all = good + neutral + bad
+  const total = good - bad
+  const average = total/all
+  const positive = good / all * 100
+  console.log(total)
+
+
   return (
     <div>
       <h1>give feedback</h1>
 
       <div>
       <button onClick={handleClickGood}>good</button>
-      <button onClick={handleClickNeutral}>good</button>
-      <button onClick={handleClickBad}>good</button>
+      <button onClick={handleClickNeutral}>neutral</button>
+      <button onClick={handleClickBad}>bad</button>
       </div>
 
       <h1>statistics</h1>
@@ -37,6 +44,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
