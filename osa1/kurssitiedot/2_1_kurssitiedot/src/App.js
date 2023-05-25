b4b1eecc533f5627101ef9b7f99b39ccd978ef9d
Lefti90 <ilkka.saekkinen@gmail.com>
2023-05-25 16:54:51 +0300

    2_1 done

---
+++ osa1/kurssitiedot/2_1_kurssitiedot/src/App.js
@@ -0,0 +1,43 @@
+const Course = (props) =>{
+  console.log(props.course.parts[0].name)
+  return(
+    <div>
+      <h1>{props.course.name}</h1>
+      <p>{props.course.parts[0].name} {props.course.parts[0].exercises}</p>
+      <p>{props.course.parts[1].name} {props.course.parts[1].exercises}</p>
+      <p>{props.course.parts[2].name} {props.course.parts[2].exercises}</p>
+    </div>
+    )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,51 +1,15 @@
-import { useState } from 'react'
+import Note from './components/Note'
 
-
-const Display = (props) => {
-  return (
-    <div>{props.counter}</div>
-  )
-}
-
-const Button = (props) => { 
-  console.log(props)
-  const { handleClick, text } = props
-  return (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
-}
-
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
+const App = ({ notes }) => {
   return (
     <div>
-      button press history: {props.allClicks.join(' ')}
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
   )
 }
-
-const App = (props) => {
-  const [value, setValue] = useState(10)
-
-  const handleClick = () => {
-    console.log('clicked the button')
-    setValue(0)
-  }
-
-  return (
-    <div>
-      {value}
-      <button onClick={handleClick}>button</button>
-    </div>
-  )
-}
-
 export default App
