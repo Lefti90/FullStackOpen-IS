d53f822f467b4ccc038d3ceb120010ac68c16a16
Lefti90 <ilkka.saekkinen@gmail.com>
2023-05-29 18:57:26 +0300

    2_9 done

--- osa2/puhelinluettelo/2_6_puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/2_6_puhelinluettelo/src/App.js
@@ -9,10 +9,14 @@ const Person = (props) =>{
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {id:1, name: 'Arto Hellas', number:'040-1231244'}
-  ]) 
+    { id: 1, name: 'Arto Hellas', number: '040-123456' },
+    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setFilter] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -23,6 +27,14 @@ const App = () => {
     setNewNumber(event.target.value)
     //console.log(event.target.value)
   }
+
+  const handleFilterChange = (event) =>{
+    setFilter(event.target.value)
+  }
+
+  const filteredPersons = persons.filter((person) =>
+  person.name.toLowerCase().includes(newFilter.toLowerCase())
+)
 
   const addName = (event) => {
     event.preventDefault()
@@ -45,6 +57,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with <input value={newFilter} onChange={handleFilterChange}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -56,7 +72,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {filteredPersons.map((person) => (
         <Person key={person.id} name={person.name} number={person.number} />
       ))}
     </div>
