d2115f955bfb9eaee909444a98c1344a3905f73f
Lefti90 <ilkka.saekkinen@gmail.com>
2023-06-04 23:20:31 +0300

    3_13 start

--- osa3_9/src/components/ServerModule.js
+++ osa3_9/src/components/ServerModule.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://render-test-u0up.onrender.com/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
