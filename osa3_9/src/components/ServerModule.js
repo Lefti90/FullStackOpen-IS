28140ce9f90cd682fb0ac45e0519418fc09db84f
Lefti90 <ilkka.saekkinen@gmail.com>
2023-06-04 23:53:49 +0300

    new build with new base url

--- osa3_9/src/components/ServerModule.js
+++ osa3_9/src/components/ServerModule.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://render-test-u0up.onrender.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
