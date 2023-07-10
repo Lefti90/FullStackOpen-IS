c34ed54c83fce1ff014c825bfa7f8b20d433dc9b
Lefti90 <ilkka.saekkinen@gmail.com>
2023-07-10 23:24:49 +0300

    Osa 5. all done

--- osa5/cypress/e2e/blog_app.cy.js
+++ osa5/cypress/e2e/blog_app.cy.js
@@ -126,4 +126,55 @@ describe('Blog ', function() {
     cy.get('#toggleDetails').click()
     cy.get('#deleteButton').should('not.exist')
   })
+
+  it.only('Sort by likes works as intended', function(){
+    //This test is very janky, I know
+    //post some blogs
+    cy.contains('Create').click()
+    cy.get('#title').type('Testblog')
+    cy.get('#author').type('John Doe')
+    cy.get('#url').type('johndoeblog.com')
+
+    cy.get('#createBlog').click()
+    //Refresh page for blog to show up
+    cy.visit('http://localhost:3000')
+
+    cy.contains('Create').click()
+    cy.get('#title').type('Testblog2')
+    cy.get('#author').type('John Doe2')
+    cy.get('#url').type('johndoeblog2.com')
+
+    cy.get('#createBlog').click()
+    //Refresh page for blog to show up
+    cy.visit('http://localhost:3000')
+
+    cy.contains('Create').click()
+    cy.get('#title').type('Testblog3')
+    cy.get('#author').type('John Doe3')
+    cy.get('#url').type('johndoeblog3.com')
+
+    cy.get('#createBlog').click()
+    //Like some blogs
+    //Refresh page for blog to show up
+    cy.visit('http://localhost:3000')
+
+    cy.get('#toggleDetails').contains('Show details').click()
+    cy.contains('Show details').click()
+    cy.contains('Show details').click()
+    cy.get('.blog #likeButton').eq(0).click()
+    cy.get('.blog #likeButton').eq(0).click()
+    cy.get('.blog #likeButton').eq(2).click()
+
+    cy.visit('http://localhost:3000')
+    cy.get('#sortButton').click()
+
+    cy.get('#toggleDetails').contains('Show details').click()
+    cy.contains('Show details').click()
+    cy.contains('Show details').click()
+
+    //Compare the likes
+    cy.get('.blog #likesDiv').eq(0).contains('Likes: 2')
+    cy.get('.blog #likesDiv').eq(1).contains('Likes: 1')
+    cy.get('.blog #likesDiv').eq(2).contains('Likes: 0')
+  })
 })
